9de47b5e4b262807d9a5ebf2fc0bb20e1e0561f6
zoyapa <zoyannnn@gmail.com>
2021-06-21 19:36:15 +0300

    functional_api2

--- functional api.py
+++ functional api.py
@@ -6,11 +6,13 @@ from keras.optimizers import Adam
 from keras.regularizers import l1
 from tensorflow.keras.datasets import cifar10
 from keras.layers import Conv2D, Flatten,concatenate
+import matplotlib.pyplot as plt
 
 gray_images=np.load('gray_images1.npy', mmap_mode='r')
 gray_images.shape
 data = cifar10.load_data()
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+images = X_train
 X_train[1].shape
 X_train.shape
 X_test.shape
@@ -37,9 +39,17 @@ model.summary()
 model.compile(optimizer='adam',loss='mse')
 
 y_train_10[0:500].shape
-model.fit(x=[y_train_10[0:500],gray_images[0:500]/255],y=pixels[0:500]/255, batch_size=500, epochs=2000)
+for i in range(0,50000,500):
+    print(i)
+    model.fit(x=[y_train_10[i:i+500],gray_images[i:i+500]/255],y=pixels[i:i+500]/255, batch_size=500, epochs=10)
 pixels.shape
-check = model.predict([y_train_10[502:503],gray_images[502:503]/255])
-plt.imshow(images[502], vmin=0, vmax=1)
-#plt.imshow(check[0].reshape(32,32,3), vmin=0, vmax=1)
+check = model.predict([y_train_10[700:701],gray_images[700:701]/255])
+check[0]
+
+
+
+plt.imshow(images[700], vmin=0, vmax=1)
+plt.show()
+
+plt.imshow(check[0].reshape(32,32,3), vmin=0, vmax=1)
 plt.show()
